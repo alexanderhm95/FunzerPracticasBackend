81870c28f42fe4bd82bace87c06f923dcf12f0f8
alexanderhm95 <byron.herrera@unl.edu.ec>
2022-06-20 23:41:08 -0500

    Ejercicio integrando un constructor al objeto

--- 05_POO/clases_objetos.js
+++ 05_POO/clases_objetos.js
@@ -2,16 +2,18 @@ class Persona{
     nombre;
     edad;
 
+    constructor(nombre, edad){
+        this.nombre = nombre;
+        this.edad = edad; 
+    }
+
     imprimir(){
         console.log(`${this.nombre} tiene ${this.edad} años`);
     }
 }
 
-const p1= new Persona();
-p1.nombre = 'Juan';
-p1.edad = 20;
-const p2= new Persona();
-p2.nombre = 'Pedro';
-p2.edad = 30;
+const p1= new Persona('Juan', 20);
+const p2= new Persona('Pedro', 30);
 
 console.log(p1);
+console.log(p2);
